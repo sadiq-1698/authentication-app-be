14d514647ac82dfea95d22c6f9280bbd7ecbaea3
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-08-07 21:10:08 +0530

    routing user controllerrs

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,7 @@ const router = express.Router();
 
 const UserController = require("../controllers/user");
 
+router.post("/register", UserController.register_new_user);
+router.post("/login", UserController.user_login);
+
 module.exports = router;
