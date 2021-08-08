f7906302c5472467c7ac6bdc0b78cd27eb25aaea
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-08-08 09:41:53 +0530

    added user edit controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,13 @@
 const userModel = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const BCRYPT_SALT_ROUNDS = 12;
 
@@ -12,7 +19,7 @@ function sendResponse(res, statusCode, message, payload = null) {
   res.send(resObject);
 }
 
-const allowedUserInfo = ["name", "email", "profilePhoto", "bio"];
+const allowedUserInfo = ["name", "email", "profilePhoto", "bio", "phone"];
 
 exports.register_new_user = async (req, res) => {
   const { email, password } = req.body;
@@ -69,5 +76,60 @@ exports.user_login = async (req, res) => {
     });
   } catch (error) {
     return sendResponse(res, 404, "An error occured. Try Again");
+  }
+};
+
+exports.edit_profile = async (req, res) => {
+  try {
+    const imagePath = await req?.files?.profilePhoto;
+    const formBody = req.body;
+
+    let secureImageURL = "";
+
+    if (imagePath) {
+      await cloudinary.uploader.upload(imagePath.tempFilePath, function(
+        error,
+        result
+      ) {
+        if (error) {
+          return sendResponse(res, 404, "Failed updating profile data");
+        }
+        if (result) {
+          secureImageURL = result.secure_url;
+        }
+      });
+    }
+
+    const userExists = await userModel.findOne({ _id: req.user.id });
+
+    if (!userExists) {
+      return sendResponse(res, 404, "User does not exist");
+    }
+
+    await userModel.findById(req.user.id, (err, updatedDetails) => {
+      if (err) return sendResponse(res, 401, "Error updating details");
+
+      Object.entries(formBody).forEach(([key, value]) => {
+        updatedDetails[key] = value;
+      });
+
+      updatedDetails.profilePhoto = secureImageURL;
+      updatedDetails.save();
+
+      let userInfo = {};
+
+      for (let i = 0; i < allowedUserInfo.length; i++) {
+        if (updatedDetails[allowedUserInfo[i]]) {
+          userInfo[allowedUserInfo[i]] = updatedDetails[allowedUserInfo[i]];
+        }
+      }
+
+      return sendResponse(res, 200, "Profile updated successfully", {
+        userInfo: userInfo
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    return sendResponse(res, 404, error);
   }
 };
