c555dd1e777f2c3ab7b71928dc792e31112c89e9
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-08-08 09:42:40 +0530

    updated user routes

--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,6 @@ const UserController = require("../controllers/user");
 router.post("/register", UserController.register_new_user);
 router.post("/login", UserController.user_login);
 
+router.patch("/edit", checkAuth, UserController.edit_profile);
+
 module.exports = router;
