18799c01581a91843129fc1378ea222444730078
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-08-08 18:15:14 +0530

    updating message model schema

--- models/message.js
+++ models/message.js
@@ -3,13 +3,16 @@ const mongoose = require("mongoose");
 const MessageSchema = new mongoose.Schema(
   {
     conversationId: {
-      type: String
+      type: String,
+      required: true
     },
-    sender: {
-      type: String
+    senderId: {
+      type: String,
+      required: true
     },
     text: {
-      type: String
+      type: String,
+      required: true
     }
   },
   {
